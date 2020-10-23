6938a762889b5e8a6d251b2dfd078657bd6ff009
HsuXwAsTe <72607204+HsuXwAsTe@users.noreply.github.com>
2020-10-24 05:03:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
